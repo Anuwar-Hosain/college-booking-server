cab82aee7afa8a65092575a9a1c6ff2122aaa43b
Anuwar-Hosain <anuwarhossn@gmail.com>
2023-07-24 18:44:58 +0600

    all api done

--- index.js
+++ index.js
@@ -36,12 +36,65 @@ async function run() {
     const reviewsCollection = client
       .db("anuBooking-college")
       .collection("reviews");
+    const usersCollection = client.db("anuBooking-college").collection("users");
+    // user post
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    // my user
+    app.get("/user/:email", async (req, res) => {
+      const jobs = await usersCollection
+        .find({
+          email: req.params.email,
+        })
+        .toArray();
+      res.send(jobs);
+    });
+    //user update
+    app.put("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: body.name,
+          email: body.email,
+          university: body.university,
+          address: body.address,
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
 
     // colleges
     app.get("/colleges", async (req, res) => {
       const result = await collegesCollection.find().toArray();
       res.send(result);
     });
+    // college search
+    app.get("/collegeSearch/:text", async (req, res) => {
+      const text = req.params.text;
+      const result = await collegesCollection
+        .find({
+          college_name: { $regex: text, $options: "i" },
+        })
+        .toArray();
+      res.send(result);
+    });
+
     // research
     app.get("/research", async (req, res) => {
       const result = await researchCollection.find().toArray();
