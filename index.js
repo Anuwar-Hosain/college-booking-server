b8b8a9cbe6122af2225a472f3ad81f3cb8e83a18
Anuwar-Hosain <anuwarhossn@gmail.com>
2023-07-24 14:14:51 +0600

    reviews done

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const jwt = require("jsonwebtoken");
 const app = express();
 const cors = require("cors");
 
@@ -11,8 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.oattrlg.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oattrlg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +25,64 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const collegesCollection = client
+      .db("anuBooking-college")
+      .collection("colleges");
+    const researchCollection = client
+      .db("anuBooking-college")
+      .collection("research");
+    const applyCollection = client.db("anuBooking-college").collection("apply");
+    const reviewsCollection = client
+      .db("anuBooking-college")
+      .collection("reviews");
+
+    // colleges
+    app.get("/colleges", async (req, res) => {
+      const result = await collegesCollection.find().toArray();
+      res.send(result);
+    });
+    // research
+    app.get("/research", async (req, res) => {
+      const result = await researchCollection.find().toArray();
+      res.send(result);
+    });
+    // post data
+    app.post("/applyForm", async (req, res) => {
+      const body = req.body;
+      const result = await applyCollection.insertOne(body);
+      res.send(result);
+    });
+    // view details
+    app.get("/college/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await collegesCollection.findOne(filter);
+      res.send(result);
+    });
+
+    // my college
+    app.get("/apply-college/:email", async (req, res) => {
+      const jobs = await applyCollection
+        .find({
+          email: req.params.email,
+        })
+        .toArray();
+      res.send(jobs);
+    });
+
+    // post review
+    app.post("/review", async (req, res) => {
+      const body = req.body;
+      const result = await reviewsCollection.insertOne(body);
+      res.send(result);
+    });
+    // colleges
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +90,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
